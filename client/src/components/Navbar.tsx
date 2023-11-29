a8b935151318ebc1ad4009f5f07f4205e0e5f6c2
Kealen-Pillay <kealenpillay@gmail.com>
2023-11-29 20:27:30 +1300

    Added Navbar.tsx

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex flex-row items-center justify-center bg-[#1e1e1e] w-full h-16">
+      <h1 className="font-bold text-2xl italic">MAFIA</h1>
+    </div>
+  );
+};
+
+export default Navbar;
