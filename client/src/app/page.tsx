b712254cb8ccb07a1f86cda50328c7a1b2d3849f
Kealen-Pillay <kealenpillay@gmail.com>
2023-12-02 15:22:44 +1300

    Refactored image and button colours

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,28 +1,27 @@
 import Link from "next/link";
+import MafiaLogo from "../../public/Mafia.png";
+import Image from "next/image";
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full mt-20">
-      <img
-        src="https://cdn4.iconfinder.com/data/icons/mafia-and-gangster/500/mafia-gangster-brutal_2-512.png"
-        alt="Mafia Icon"
-        className="w-9/12 aspect-square"
-      />
+      <Image src={MafiaLogo} alt="Mafia Icon" className="w-4/5 mb-10 ml-10" />
       <input
         type="text"
         placeholder="Username"
-        className="w-9/12 h-10 my-3.5 rounded p-2 text-center font-medium italic text-black border-2 border-[#848282]"
+        className="w-9/12 h-10 my-5 rounded p-2 text-center font-medium italic text-black border-2 border-[#848282]"
       />
       <Link
         href="/host"
-        className="bg-[#8F86FC] text-black w-9/12 h-10 font-bold rounded flex justify-center items-center my-7 border-2 border-black">
-          Host Game
-        </Link>
+        className="bg-[#C1BCFF] text-black w-9/12 h-10 font-bold rounded flex justify-center items-center my-7 border-2 border-[#8F86FC]"
+      >
+        <p className="text-[#8F86FC]">Host Game</p>
+      </Link>
       <Link
         href="/join"
-        className="bg-[#53ED9A] text-black w-9/12 h-10 font-bold rounded flex justify-center items-center border-2 border-black"
+        className="bg-[#C5FFE0] text-black w-9/12 h-10 font-bold rounded flex justify-center items-center border-2 border-[#53ED9A]"
       >
-        Join Game
+        <p className="text-[#53ED9A]">Join Game</p>
       </Link>
     </div>
   );
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="flex flex-row items-center justify-center bg-[#1e1e1e] w-full h-16">
-      <h1 className="font-bold text-2xl italic">MAFIA</h1>
+      <h1 className="font-bold text-3xl italic">MAFIA</h1>
     </div>
   );
 };
