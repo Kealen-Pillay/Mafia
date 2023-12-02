ab5a2df929f0e0ba4cdf458032014a4796be098c
Kealen-Pillay <kealenpillay@gmail.com>
2023-12-02 17:11:03 +1300

    Added loading indicator to lobby players

--- client/src/app/host/page.tsx
+++ client/src/app/host/page.tsx
@@ -2,60 +2,88 @@
 import React, { useState } from "react";
 import Face6RoundedIcon from "@mui/icons-material/Face6Rounded";
 import Face3RoundedIcon from "@mui/icons-material/Face3Rounded";
+import Link from "next/link";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const HostScreen = () => {
-  const [roomCode, setRoomCode] = useState<string>("7HI6G1");
+  const [roomCode, setRoomCode] = useState<string>("#7HI6G1");
+  const [players, setPlayers] = useState<string[] | null>(null);
 
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
-      <div className="flex font-bold flex-row text-2xl italic my-3 px-5">
-        <h1>Room Code: {roomCode}</h1>
+      <div className="flex font-bold flex-row text-2xl my-5 w-full items-center">
+        <div className="w-1/5 px-5">
+          <Link href="/">
+            <ArrowBackIosIcon />
+          </Link>
+        </div>
+        <div className="flex flex-row space-x-2">
+          <h1>Room Code: </h1>
+          <h1 className="text-red-500">{roomCode}</h1>
+        </div>
       </div>
-      <div className="grid grid-cols-4 rounded-lg gap-4 bg-white p-3 w-11/12 aspect-square">
-        <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
-          {" "}
-          <Face6RoundedIcon className="text-4xl" />{" "}
+      {players ? (
+        <div className="grid grid-cols-4 rounded-lg gap-4 bg-white p-3 w-11/12 h-fit">
+          <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
+            {" "}
+            <Face6RoundedIcon className="text-4xl" />{" "}
+          </div>
+          <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
+            {" "}
+            <Face6RoundedIcon className="text-4xl" />{" "}
+          </div>
+          <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
+            {" "}
+            <Face3RoundedIcon className="text-4xl" />{" "}
+          </div>
+          <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
+            {" "}
+            <Face6RoundedIcon className="text-4xl" />{" "}
+          </div>
+          <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
+            {" "}
+            <Face3RoundedIcon className="text-4xl" />{" "}
+          </div>
+          <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
+            {" "}
+            <Face3RoundedIcon className="text-4xl" />{" "}
+          </div>
+          <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
+            {" "}
+            <Face6RoundedIcon className="text-4xl" />{" "}
+          </div>
+          <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
+            {" "}
+            <Face3RoundedIcon className="text-4xl" />{" "}
+          </div>
+          <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
+            {" "}
+            <Face6RoundedIcon className="text-4xl" />{" "}
+          </div>
+          <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
+            {" "}
+            <Face3RoundedIcon className="text-4xl" />{" "}
+          </div>
+          <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
+            {" "}
+            <Face6RoundedIcon className="text-4xl" />{" "}
+          </div>
+          <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
+            {" "}
+            <Face3RoundedIcon className="text-4xl" />{" "}
+          </div>
         </div>
-        <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
-          {" "}
-          <Face6RoundedIcon className="text-4xl" />{" "}
-        </div>
-        <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
-          {" "}
-          <Face3RoundedIcon className="text-4xl" />{" "}
-        </div>
-        <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
-          {" "}
-          <Face6RoundedIcon className="text-4xl" />{" "}
-        </div>
-        <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
-          {" "}
-          <Face3RoundedIcon className="text-4xl" />{" "}
-        </div>
-        <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
-          {" "}
-          <Face3RoundedIcon className="text-4xl" />{" "}
-        </div>
-        <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
-          {" "}
-          <Face6RoundedIcon className="text-4xl" />{" "}
-        </div>
-        <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
-          {" "}
-          <Face3RoundedIcon className="text-4xl" />{" "}
-        </div>
-        <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
-          {" "}
-          <Face6RoundedIcon className="text-4xl" />{" "}
-        </div>
-        <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
-          {" "}
-          <Face3RoundedIcon className="text-4xl" />{" "}
-        </div>
-        <div className="rounded-full border-2 border-black aspect-square text-black flex justify-center items-center">
-          {" "}
-          <Face6RoundedIcon className="text-4xl" />{" "}
-        </div>
+      ) : (
+        <CircularProgress />
+      )}
+      <div className="w-full flex justify-center my-5">
+        <Link
+          href="/"
+          className="bg-[#C5FFE0] text-black w-11/12 h-12 font-bold rounded flex justify-center items-center border-2 border-[#31CC78]"
+        >
+          <p className="text-[#31CC78]">Start Game</p>
+        </Link>
       </div>
     </div>
   );
--- client/src/app/join/page.tsx
+++ client/src/app/join/page.tsx
@@ -1,9 +1,17 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import MafiaLogo from "../../../public/Mafia.png";
+import { useState } from "react";
 
 const JoinScreen = () => {
+  const [username, setUsername] = useState<string>("");
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setUsername(e.target.value);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
       <div className="flex w-screen h-10 items-center pl-3 pt-2">
@@ -17,9 +25,11 @@ const JoinScreen = () => {
         className="w-4/5 mb-10 ml-10  mt-20"
       />
       <input
+        value={username}
         type="text"
         placeholder="Room Code"
         className="w-9/12 h-14 my-10 rounded p-2 text-center font-medium italic text-black border-2 border-[#848282]"
+        onChange={onChange}
       />
       <Link
         href="/"
